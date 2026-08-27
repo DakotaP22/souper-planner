72922106247b97c93be6b2076d817ab7b3c43023
DakotaP22 <peased22@gmail.com>
2026-08-27 05:03:05 +0000

    feat: Adds header

--- src/app/shared/layout/app-layout/app-layout.ts
+++ src/app/shared/layout/app-layout/app-layout.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, RouterLink],
   selector: 'app-layout',
   styles: ``,
   template: ` 
-    <p>app-layout works!</p>
+    <mat-toolbar>
+      <span>Souper Planner</span>
+
+      <span class="flex-1"></span>
+
+      <a mat-button routerLink="/home">Home</a>
+      <a mat-button routerLink="/meals">Meals</a>
+      <a mat-button routerLink="/plan">Plan</a>
+    </mat-toolbar>
     <router-outlet />
   `,
 })
