bdb2b63093f23a8ba6034e5fb87d7279cdb93256
DakotaP22 <peased22@gmail.com>
2026-08-27 05:05:36 +0000

    feat: Add nav buttons to home page

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [MatButtonModule, RouterLink],
   selector: 'home',
   styles: ``,
-  template: ` <p>home works!</p> `,
+  template: ` 
+    <span class="flex-1">
+      <a mat-button routerLink="/meals">Explore Meals</a>
+      <a mat-button routerLink="/plan">Start Planning</a>
+    </span>
+  `,
 })
 export class Home {}
